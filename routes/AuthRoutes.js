9217483c40a236c3ddbfc6f8eb834a4ac37eb37b
laxmibagodi <laxmibagodi25@gmail.com>
2024-11-25 11:00:56 +0530

    loginAndRegister

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import {registerUser} from "../controller/AuthController.js";
+import {loginUser, registerUser} from "../controller/AuthController.js";
 
 const router = express.Router()
 
 router.post('/register', registerUser)
+router.post('/login', loginUser)
 export default router
